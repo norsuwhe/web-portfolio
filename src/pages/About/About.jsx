ab6ec6517ced25b2f0f07e758ece8cd073a4bf9c
Vladimir Semenov <vladimirsemionov796@gmail.com>
2022-11-18 18:15:46 +0500

    small layout fix

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,26 @@
+import MyImage from "../../assets/img/photo.png";
+import "./About.scss";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="about">
+      <div className="about__description">
+        <h1 className="about__header">Привет, меня зовут Владимир</h1>
+        <h2 className="about__spec">И я Front-end разработчик</h2>
+        <h3 className="about__tech-stack">TypeScript / React / Redux / Sass</h3>
+        <p className="about__paragraph">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi.
+          Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
+          mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
+          tellus..
+        </p>
+      </div>
+      <div className="about__image-group">
+        <img className="about__image" src={MyImage} alt="" />
+        <div className="about__rectangle"></div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,5 +1,30 @@
+import "./Contacts.scss";
+
 const Contacts = () => {
-  return <div>Contacts</div>;
+  return (
+    <section>
+      <ul>
+        <li>
+          <a href="">Email</a>
+        </li>
+        <li>
+          <a href="">Telegram</a>
+        </li>
+        <li>
+          <a href="">HH</a>
+        </li>
+        <li>
+          <a href="">GitHub</a>
+        </li>
+      </ul>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi.
+        Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
+        mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
+        tellus..
+      </p>
+    </section>
+  );
 };
 
 export default Contacts;
